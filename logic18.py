7e97e023a8a5194310d1fef9bb9da1416ae7ff44
Bahtee <bahtiyorjon_89@mail.ru>
2022-12-27 02:17:18 +0500

    18

--- logic18.py
+++ logic18.py
@@ -5,4 +5,15 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    x1 = a % 10 
+    x2 = a % 100
+    x2 = x2 // 10
+    x3 = a % 1000
+    x3 = x3 // 100
+    x4 = a % 10000
+    x4 = x4 // 1000
+    x5 = a // 10000
+    
+    return x5 < x4 < x3 < x2 < x1
+print(main(12347))
+    
